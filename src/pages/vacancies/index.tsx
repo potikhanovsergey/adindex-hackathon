816bc637dfe2509e26b48cd6e8e430035d5b73db
Darya Syomina <d.semina10@gmail.com>
2023-04-26 12:37:06 +0300

    vacancies page

--- src/pages/vacancies/index.tsx
+++ src/pages/vacancies/index.tsx
@@ -1,11 +1,34 @@
 import Layout from "src/core/layouts/Layout"
 import { BlitzPage } from "@blitzjs/next"
-import { Title } from "@mantine/core"
+import { Stack, Title } from "@mantine/core"
+import { useQuery } from "@blitzjs/rpc"
+import VacancyCard, { ExtendedVacancy } from "src/vacancy/components/VacancyCard"
+import getVacancies from "src/vacancy/queries/getVacancies"
+import { Suspense } from "react"
+
+const AllVacancies = () => {
+  const [vacancies] = useQuery(getVacancies, {
+    include: {
+      company: true,
+    },
+  })
+
+  return (
+    <Stack>
+      {vacancies.map((vacancy) => (
+        <VacancyCard vacancy={vacancy as ExtendedVacancy} key={vacancy.id} withUserButton={true} />
+      ))}
+    </Stack>
+  )
+}
 
 const VacanciesPage: BlitzPage = () => {
   return (
     <Layout>
-      <Title>Вакансии</Title>
+      <Title mb="md">Вакансии</Title>
+      <Suspense>
+        <AllVacancies />
+      </Suspense>
     </Layout>
   )
 }
